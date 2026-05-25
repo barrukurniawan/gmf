f3e2a2b333c4fb81c0e9588b9da40a61c2d3a9ec
Barru Kurniawan <barru@motionpay.id>
2026-05-25 14:04:11 +0700

    updated code

--- app/Views/default/company_left_menu.php
+++ app/Views/default/company_left_menu.php
@@ -75,7 +75,7 @@ $setup_modules = unserialize($xin_com_system['setup_modules']);
     </span> </a> </li>
   <?php } ?>
   <!-- Regulation Portal -->
-  <li class="pc-item <?php if($router->controllerName() == '\App\Controllers\Erp\Regulation')echo 'active';?>"> <a href="<?= site_url('erp/regulation-portal');?>" class="pc-link" style="align-items:flex-start;"> <span class="pc-micon" style="padding-top:2px;"><i data-feather="book"></i></span><span class="pc-mtext" style="white-space:normal;line-height:1.4;">
+  <li class="pc-item <?php if($router->controllerName() == '\App\Controllers\Erp\Regulation')echo 'active';?>"> <a href="<?= site_url('erp/regulation-portal');?>" class="pc-link" style="display:flex !important; align-items:flex-start;"> <span class="pc-micon" style="flex-shrink:0; padding-top:3px;"><i data-feather="book"></i></span><span class="pc-mtext" style="flex:1; min-width:0; white-space:normal; line-height:1.4;">
     Company Manual Publication
     </span> </a> </li>
     <?php if(isset($setup_modules['training'])): if($setup_modules['training']==1):?>
